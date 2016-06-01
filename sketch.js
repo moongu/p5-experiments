b49b665fa8a91bae30a8f478f8ff2b2acd021f8b
y <zoey.jeong@gmail.com>
2016-06-01 17:45:54 -0400

    i think this works

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 /*	First experiment with P5
 	Matrix rain	*/
 
-var squaresArray = [];
 var textArray = [];
+var trailLen = 10;
 
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight);
 
-	for(var i=0; i<50; i++){
+	for(var i=0; i<100; i++){
 		var t = new FallingText();
 		textArray.push(t);
 	}
@@ -25,9 +25,9 @@ function draw() {
 
 		text(floor(random(0,2)).toString(), tx.x, tx.y);
 
-		for(var i=0; i<=8; i++){
+		for(var i=0; i<=trailLen; i++){
 			fill(0, green-=20, 0);
-			text(floor(random(0,2)).toString(), tx.x, yPos-=30);
+			text(floor(random(0,2)).toString(), tx.x, yPos-=tx.fontSize);
 		}
 
 		tx.move();
@@ -38,9 +38,10 @@ function FallingText() {
 	this.y = random(0, window.innerHeight);
 	this.x = random(0, window.innerWidth);
 	this.fontSize = 30;
-	this.clor = color(0,200,0);
+	this.speed = 3;
 
 	this.move = function() {
-		(this.y >= (window.innerHeight + 30*9) ? this.y = -30 : this.y += 3);
+		(this.y >= (window.innerHeight + this.fontSize*trailLen) ? 
+			this.y = -this.fontSize : this.y += this.speed);
 	}
 }
