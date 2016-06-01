d5d63284b2d5d200d3bb4065901fdd44b7d315e8
y <zoey.jeong@gmail.com>
2016-06-01 15:55:23 -0400

    columns of fading squares falling continuously

--- sketch.js
+++ sketch.js
@@ -17,12 +17,21 @@ function draw() {
 	fill(0, 100, 0);
 
 	squaresArray.forEach(function(square) {
-		fill(square.color);
+		var green = 200;
+		var yPos = square.y;
+
+		fill(0, green, 0);
+		stroke(0);
 		rect(square.x, square.y, square.width, square.height);
+
+		for(var i=0; i<=8; i++){
+			fill(0, green-=20, 0);
+			rect(square.x, yPos-=30, square.width, square.height);
+		}
+
 		square.move();
 	});
 }
-
 
 
 function FallingSquare() {
@@ -33,6 +42,6 @@ function FallingSquare() {
 	this.color = color(0,200,0);
 
 	this.move = function() {
-		(this.y >= window.innerHeight ? this.y = -30 : this.y += 3);
+		(this.y >= (window.innerHeight + 30*9) ? this.y = -30 : this.y += 3);
 	}
 }
